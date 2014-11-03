6ac0a9b19e106e25b8b92abd5dc400f2894d0cde
Amit Jakubowicz <ajk@voxelbrain.com>
2014-11-03 10:34:26 +0100

    Updates

--- src/app/app.js
+++ src/app/app.js
@@ -7,3 +7,12 @@ angular.module('angularBase', ['ui.router', 'ng-polymer-elements'])
     })
     .controller('AppController', function($scope) {});
 ;
+
+window.addEventListener('polymer-ready', function() {
+    var ngApp = document.getElementById('ng-app').getAttribute('to-be-ng-app');
+    if (!ngApp) {
+        console.warn('no ng-app');
+    }
+    console.log('readt');
+    angular.bootstrap(wrap(document), [ngApp]);
+});
